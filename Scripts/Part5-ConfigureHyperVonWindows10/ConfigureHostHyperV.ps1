8e6b1032159fa4009ce4b32bd8f2a539025147e7
hedaprakash <prakash@starinnovative.com>
2020-09-14 22:55:10 +0000

    Add files via upload
    Part5-ConfigureHyperVonWindows10

---
+++ Scripts/Part5-ConfigureHyperVonWindows10/ConfigureHostHyperV.ps1
@@ -0,0 +1,82 @@
+﻿exit
+#Save powershell to taskbar
+
+Rename-Computer w10demo
+
+Restart-Computer 
+
+systeminfo.exe
+
+Set-ExecutionPolicy Unrestricted -Force
+Set-ExecutionPolicy bypass -Force
+
+# install RDP visionapp
+
+#win10
+Enable-WindowsOptionalFeature –Online –FeatureName  TelnetClient 
+
+Get-Service -Name WinRM 
+Set-Service -Name WinRM -StartupType Automatic
+Get-Service -Name WinRM | start-Service
+Get-Service -Name WinRM 
+
+Get-Item -Path WSMan:\localhost\Client\TrustedHosts 
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value * -Concatenate -Force
+Get-Item -Path WSMan:\localhost\Client\TrustedHosts 
+
+"Disable firewall" 
+netsh advfirewall set allprofiles state off
+
+#Disable-UAC
+$SystemKey = “HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System”
+$UACAleadySet=Get-ItemProperty -Path $SystemKey -Name "EnableLUA"  -ErrorAction Continue
+$UACAleadySet = $UACAleadySet.EnableLUA
+Set-ItemProperty -Path $SystemKey -Name "EnableLUA" -Value 0  -Force
+Stop-Process -Name Explorer -Force
+Start-Process Explorer
+
+Restart-Computer 
+
+#Run on windows 10
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All
+# system will restart now
+
+Restart-Computer 
+
+#after reboot
+Set-VMhost -EnableEnhancedSessionMode $TRUE
+
+Get-NetAdapter
+Get-NetAdapter |?{$_.Status -eq "Up"}
+
+
+New-VMSwitch -Name lan10 -SwitchType Internal -Notes "lan10 network"
+New-VMSwitch -Name lan20 -SwitchType Internal -Notes "lan20 network"
+New-VMSwitch -Name lanNet -NetAdapterName (Get-NetAdapter |?{$_.Status -eq "Up" -and !$_.Virtual}).Name -Notes "lanNet"
+
+Get-NetAdapter |?{$_.Status -eq "Up"}| Sort-Object -Property Virtual,InterfaceDescription
+
+Rename-NetAdapter -Name "vEthernet (lan10)" -NewName Lan10
+Rename-NetAdapter -Name "vEthernet (lan20)" -NewName lan20
+Rename-NetAdapter -Name "vEthernet (lanNet)" -NewName lanNet
+
+Get-NetAdapter |?{$_.Status -eq "Up"}| Sort-Object -Property Virtual,InterfaceDescription| select name,ifindex
+
+#disable IPV6
+Disable-NetAdapterBinding -Name "*lan*" –ComponentID ms_tcpip6
+
+#Setup Lan10 with right IP
+Get-NetIPAddress -InterfaceIndex 61
+New-NetIPAddress -InterfaceIndex 61 -IPAddress 10.10.10.3 -PrefixLength 24
+
+#Setup Lan20 with right IP
+Get-NetAdapter |?{$_.Status -eq "Up"}| Sort-Object -Property Virtual,InterfaceDescription | select name,ifindex
+Get-NetIPAddress -InterfaceIndex 71
+New-NetIPAddress -InterfaceIndex 71 -IPAddress 20.20.20.3 -PrefixLength 24
+Get-NetIPAddress -InterfaceIndex 71
+
+# lanNet
+Get-NetIPAddress -InterfaceIndex 79
+
+Restart-Computer 
+
